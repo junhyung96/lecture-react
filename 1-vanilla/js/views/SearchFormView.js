8714576dfcc34053d2ed1213c35c6da5c9950b34
junhyung <wnsgud0895@gmail.com>
2023-12-30 11:57:55 +0900

    search-form-1 completed

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,4 +1,4 @@
-import { qs } from "../helpers.js";
+import { on, qs } from "../helpers.js";
 import View from "./View.js";
 
 const tag = "[SearchFormView]";
@@ -8,14 +8,27 @@ export default class SearchFormView extends View {
     console.log(tag, "constructor");
 
     super(qs("#search-form-view"));
-
+    
+    this.inputElement = qs("[type=text]", this.element)
     this.resetElement = qs("[type=reset]", this.element);
 
     this.showResetButton(false);
-    // TODO
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
   }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", ()=> this.handleKeyup())
+  }
+
+  handleKeyup() {
+    console.log(tag, "handleKeyup", this.inputElement.value)
+    const {value} = this.inputElement
+    this.showResetButton(value.length > 0)
+  }
+
+
 }
